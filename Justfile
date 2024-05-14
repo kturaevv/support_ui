75aec339dc5d0ca8ca369738896b2d6950ae5156
kturaevv <kturaevv@gmail.com>
2024-05-14 16:30:59 +0500

    state: project setup

---
+++ Justfile
@@ -0,0 +1,12 @@
+# Production build
+build:
+    bun add -D svelte-adapter-bun
+    bun run build
+
+# Run build
+dev:
+    bun --bun run dev
+
+# Add shadcn component
+add:
+    bunx shadcn-svelte@latest add 
---
+++ init.sh
@@ -0,0 +1,8 @@
+# install Just
+wget -qO - 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | sudo tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
+echo "deb [arch=all,$(dpkg --print-architecture) signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list
+sudo apt update
+sudo apt install just
+
+# install Bun runtime
+curl -fsSL https://bun.sh/install | bash
